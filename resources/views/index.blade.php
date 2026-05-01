78522ce8a6da3946b07eb5702f93a03267f5cb14
Gabriel Herrera <gabri15042004@gmail.com>
2026-05-01 18:53:17 +0200

    Creada página de detalles(#5)
    Se ha creado la página que muestra información detallada de cada película, además se ha añadido un método de selección de fecha en la cartelera principal para mostrar las sesiones disponibles en la página de detalles.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,20 +11,27 @@
     <body style="background-color: #FFFBED;">
         <div class="container py-5">
             <div class="row row-cols-1 row-cols-md-3">
-                @foreach ($peliculas as $peli)
+                @foreach ($peliculas as $pelicula)
                 <div class="col mb-4">
                     <div class="card text-bg-dark h-100 shadow-sm">
-                        <img src="{{asset($peli->imagen_url)}}" class="card-img-top" alt="{{ $peli->titulo }}" style="height: 600px; object-fit: cover;">
+                        <img src="{{asset($pelicula->imagen_url)}}" class="card-img-top" style="height: 600px; object-fit: cover;">
                         <div class="card-body d-flex flex-column">
                             <h5 class="card-title">
-                                {{ $peli->titulo }}
+                                {{ $pelicula->titulo }}
                             </h5>
                             <p class="card-text flex-grow-1">
-                                {{ Str::limit($peli->descripcion, 200, '...') }}
+                                {{ Str::limit($pelicula->descripcion, 200, '...') }}
                             </p>
-                            <a href="#" class="btn btn-info w-100">
-                                Ver sesiones
-                            </a>
+                            <form action="{{ route('pelicula.show', $pelicula->id, ) }}" method="GET">
+                                <div class="row row-cols-md-2">
+                                    <div class="col">
+                                        <input type="date" class="form-control mb-1" id="fecha" name="fecha_sesion" required>
+                                    </div>
+                                    <div class="col">
+                                        <input type="submit" class="btn btn-info" value="Ver sesiones">
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
