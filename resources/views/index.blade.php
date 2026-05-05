19614a5b1f60d9fae133401606c1539fb14ddab7
Gabriel Herrera <gabri15042004@gmail.com>
2026-05-05 09:24:03 +0200

    Corregido pareado div del index(#5)
    Los div estaban mal pareaddos, sobraba un cierre y eso provocaba que se mostrara mal toda la página.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,29 +1,28 @@
 @include('header')
-    <body">
-        <div class="container py-5">
-            <div class="row row-cols-1 row-cols-md-3">
-                @foreach ($peliculas as $pelicula)
-                <div class="col mb-4">
-                    <div class="card text-bg-dark h-100 shadow-sm">
-                        <img src="{{asset($pelicula->imagen_url)}}" class="card-img-top" style="height: 600px; object-fit: cover;">
-                        <div class="card-body d-flex flex-column">
-                            <h5 class="card-title">
-                                {{ $pelicula->titulo }}
-                            </h5>
-                            <p class="card-text flex-grow-1">
-                                {{ Str::limit($pelicula->descripcion, 200, '...') }}
-                            </p>
-                            <form action="{{ route('pelicula.detalles', $pelicula->id, ) }}" method="GET">
-                                <div class="row g-2">
-                                    <div class="col-12 col-md-6">
-                                        <input type="date" class="form-control mb-1" id="fecha" name="fecha_sesion" required>
-                                    </div>
-                                    <div class="col-12 col-md-6">
-                                        <input type="submit" class="btn btn-info w-100" value="Ver sesiones">
-                                    </div>
+<body">
+    <div class="container py-5">
+        <div class="row row-cols-1 row-cols-md-3">
+            @foreach ($peliculas as $pelicula)
+            <div class="col mb-4">
+                <div class="card text-bg-dark h-100 shadow-sm">
+                    <img src="{{asset($pelicula->imagen_url)}}" class="card-img-top" style="height: 600px; object-fit: cover;">
+                    <div class="card-body d-flex flex-column">
+                        <h5 class="card-title">
+                            {{ $pelicula->titulo }}
+                        </h5>
+                        <p class="card-text flex-grow-1">
+                            {{ Str::limit($pelicula->descripcion, 200, '...') }}
+                        </p>
+                        <form action="{{ route('pelicula.detalles', $pelicula->id, ) }}" method="GET">
+                            <div class="row g-2">
+                                <div class="col-12 col-md-6">
+                                    <input type="date" class="form-control mb-1" id="fecha" name="fecha_sesion" required>
                                 </div>
-                            </form>
-                        </div>
+                                <div class="col-12 col-md-6">
+                                    <input type="submit" class="btn btn-info w-100" value="Ver sesiones">
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
