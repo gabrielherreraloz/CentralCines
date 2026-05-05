07550a526906274dc2025f04d8b57211f97b9385
María <mariaremacho04@gmail.com>
2026-05-05 11:32:35 +0200

    Cambios en el footer (#20) Adaptación de las columnas del footer para que aparezca todo en el centro de la pantalla.

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,20 +1,20 @@
             <footer class="bg-dark text-white text-center py-3 mt-4">
             <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-md-4 text-center text-md-start">
-                        <p class="mb-0 fw-bold">2026</p>
+                
+                    <div class="text-center">
+                        <p class="fw-bold">2026</p>
                         <small class="d-block text-secondary">María Remacho Aneas</small>
                         <small class="d-block text-secondary">Gabriel Herrera Lozano</small>
                         <small class="d-block text-secondary">Eladio Carbonell Rodríguez</small>
                     </div>
-                    <div class="col-md-4 text-center">
-                        <a href="LINKPAGINACONTACTO" class="btn btn-outline-light mx-1">Contacto</a>
-                        <a href="ENLACEAPDF" target="_blank" class="btn btn-outline-info mx-1">Informe (PDF)</a>
+                    <div class="text-center">
+                        <a href="LINKPAGINACONTACTO" class="btn btn-outline-light mx-1 my-3">Contacto</a>
+                        <a href="ENLACEAPDF" target="_blank" class="btn btn-outline-info mx-1 my-3">Informe (PDF)</a>
                     </div>
-                    <div class="col-md-4 text-center text-md-end">
+                    <div class="text-center">
                         <small class="text-secondary">Todos los derechos reservados</small>
                     </div>
-                </div>
+                
             </footer>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
