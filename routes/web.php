48587b0b3279ac04b9abef7af24c2b219a34e7d2
Gabriel Herrera <gabri15042004@gmail.com>
2026-05-12 12:46:21 +0200

    Añadida página de referencias en el frontend

--- routes/web.php
+++ routes/web.php
@@ -5,19 +5,18 @@ use App\Http\Controllers\PeliculaController;
 use App\Models\Sesion;
 use App\Models\Butaca;
 use App\Models\Entrada;
+use Erus\Parsedown;
 
 Route::get('/', [PeliculaController::class, 'index']);
 Route::get('/pelicula/{id}', [PeliculaController::class, 'detalles'])->name('pelicula.detalles');
 Route::get('/contacto', function () {return view('contacto'); })->name('contacto');
-
 Route::get('/sesion/{id}/butacas', function ($id) {
     $sesion = Sesion::findOrFail($id);
-
-    // 1. Obtenemos todas las butacas de la sala de esta sesión
     $butacas = Butaca::where('id_sala', $sesion->id_sala)->get();
-
-    // 2. Obtenemos los IDs de las butacas que ya tienen entrada para ESTA sesión
     $butacasOcupadasIds = Entrada::where('id_sesion', $id)->pluck('id_butaca')->toArray();
-
     return view('butacas', compact('sesion', 'butacas', 'butacasOcupadasIds'));
 })->name('butacas.sesion');
+Route::get('/referencias', function () {
+    $contenido = file_get_contents(base_path('Documentation/References.md'));
+    return view('references', compact('contenido'));
+})->name('references');
